09a7c35c8b9e6a5a7392a6c7b051041be3c6cc39
keanestack <74552264+keane-stack@users.noreply.github.com>
2022-09-28 09:27:03 +0000

    skelet van menu staat

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 import random
  
 from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
- 
+menu_options = {
+    1: 'Option 1',
+    2: 'Option 2',
+    3: 'Option 3',
+    4: 'Exit',
+}
+
+def print_menu():
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def option1():
+     print('Handle option \'Option 1\'')
+
+def option2():
+     print('Handle option \'Option 2\'')
+
+def option3():
+     print('Handle option \'Option 3\'')
+
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if option == 1:
+           option1()
+        elif option == 2:
+            option2()
+        elif option == 3:
+            option3()
+        elif option == 4:
+            print('Thanks message before exiting')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
  
 class MyAgent(MLAgent):
     def evaluate(self, board):
@@ -24,4 +63,4 @@ train_and_plot(
     validation_agent=random_agent,
     iterations=50,
     trainings=100,
-    validations=1000)
+    validations=900)
